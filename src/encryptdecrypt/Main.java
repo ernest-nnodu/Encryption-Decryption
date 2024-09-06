d63260895b82d2357636314530345de25dfd4bf6
ernest nnodu <ernest.nnodu@outlook.com>
2024-09-06 21:38:25 +0100

    Read input from main method arguments

--- src/encryptdecrypt/Main.java
+++ src/encryptdecrypt/Main.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        String operation = scanner.nextLine();
-        Message message = new Message(scanner.nextLine());
-        int key = scanner.nextInt();
+        String mode = "enc";
+        int key = 0;
+        String data = "";
 
-        if (operation.equals("enc")) {
+        for (int index = 0; index < args.length; index += 2) {
+            if (args[index].equals("-mode")) {
+                mode = args[index + 1];
+            } else if (args[index].equals("-key")) {
+                key = Integer.parseInt(args[index + 1]);
+            } else {
+                data = args[index + 1];
+            }
+        }
+
+        Message message = new Message(data);
+
+        if (mode.equals("enc")) {
             System.out.println(message.encryptMessageWithUnicode(new EncryptionAlgorithm(), key));
         } else {
             System.out.println(message.decryptMessageWithUnicode(new EncryptionAlgorithm(), key));
